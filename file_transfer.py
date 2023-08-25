afb877370afa259856a5c7e567cb0edbfc4fee4f
Marco Dominguez <marcod082@gmail.com>
2023-08-24 21:35:08 -0500

    another commit

--- file_transfer.py
+++ file_transfer.py
@@ -1,11 +1,4 @@
-from tqdm import tqdm, trange
-from time import sleep
 
-pbar = tqdm(total=100)
-for i in range(10):
-    sleep(0.25)
-    if i == 5:
-        pbar.reset()
-    pbar.update(12)
-    #pbar.set_description("Processing %s" % char)
-pbar.close()
+
+d = 5
+print(d + 5)
