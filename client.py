419c038a0cf0905f1e692e888e64e21cc71ccbf2
Marco Dominguez <marcod082@gmail.com>
2023-08-29 15:02:27 -0500

    implemented send/recieve

--- client.py
+++ client.py
@@ -1,7 +1,29 @@
 import socket
+import threading
 
 host = 'localhost'
-port = 9090
+
+def receive_data():
+    port = 9090
+   
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.connect((host, port))
+    msg = lsock.recv(1024).decode('utf-8')
+    print(msg)
+
+threading.Thread(target=receive_data, daemon=True).start()
+
+port = 9091
 
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.connect((host, port))
+lsock.bind((host, port))
+lsock.listen(2)
+
+s, addr = lsock.accept()
+print(f"Connected to client on {addr}")
+
+s.send(bytes("Hello from client",'utf-8'))
+s.close()
+
+input("press enter to continue...")
+
--- server.py
+++ server.py
@@ -2,26 +2,28 @@ import socket
 import threading
 import tqdm
 
-
-def receive_data(sock):
-    print('check')
-    while True:
-        print('check1')
-        msg = sock.recv(1024).decode('utf-8')
-        print('check')
-        print(msg)
-
 host = 'localhost'
+
+def receive_data():
+    port = 9091
+
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.connect((host, port))
+    msg = lsock.recv(1024).decode('utf-8')
+    print(msg)
+
 port = 9090
 
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
-
 lsock.listen(2)
 
-#while True:
 s, addr = lsock.accept()
 print(f"Connected to client on {addr}")
-threading.Thread(target=receive_data, daemon=True, args=(s,)).start()
+
+threading.Thread(target=receive_data, daemon=True).start()
+
+s.send(bytes("Welcome to server",'utf-8'))
+s.close()
 
 input("press enter to continue...")
