14436ae67a58236f5ad699d6c4d435fe1f9a6b5b
Marco Dominguez <marcod082@gmail.com>
2023-08-29 13:11:57 -0500

    added some changes

--- server.py
+++ server.py
@@ -4,8 +4,11 @@ import tqdm
 
 
 def receive_data(sock):
+    print('check')
     while True:
-        msg = sock.recv(1024)
+        print('check1')
+        msg = sock.recv(1024).decode('utf-8')
+        print('check')
         print(msg)
 
 host = 'localhost'
@@ -16,7 +19,9 @@ lsock.bind((host, port))
 
 lsock.listen(2)
 
-while True:
-    s, addr = lsock.accept()
-    print(f"Connected to client on {addr}")
-    threading.Thread(target=receive_data).start()
+#while True:
+s, addr = lsock.accept()
+print(f"Connected to client on {addr}")
+threading.Thread(target=receive_data, daemon=True, args=(s,)).start()
+
+input("press enter to continue...")
