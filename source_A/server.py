6020a390d8107d17b108261c9efa4f6fe9a57a86
Marco Dominguez <marcod082@gmail.com>
2023-08-31 13:02:21 -0500

    added new folders

--- source_A/server.py
+++ source_A/server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import tqdm
+import os
 
 host = 'localhost'
 
@@ -10,6 +11,7 @@ def receive_data():
     lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     lsock.connect((host, port))
     msg = lsock.recv(1024).decode('utf-8')
+    print('check')
     print(msg)
 
 port = 9090
@@ -22,6 +24,10 @@ s, addr = lsock.accept() #this will block the program while waiting for connecti
 print(f"Connected to client on {addr}")
 
 threading.Thread(target=receive_data, daemon=True).start()
+
+#file_name = "sendme.txt"
+#file = open(file_name,"rb")
+#file_size = os.path.getsize(file_name)
 
 s.send(bytes("Welcome to server",'utf-8'))
 
